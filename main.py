2dfe6c035de0de2fc817295f1d50f569dfea9b52
realnoobadi <workforadi0@gmail.com>
2024-11-07 12:34:45 +0530

    streamlit

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from sklearn.datasets import load_iris
+
+st.write("hello world")
+st.write('GM')
+st.balloons()
+
+data = load_iris(as_frame =True).frame
+st.write(data.head())
